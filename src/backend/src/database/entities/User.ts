da32f2aa90aaed1c289488c373dd663c6aea49a1
Elias Biondo <contato@eliasbiondo.com>
2022-11-22 15:11:28 -0300

    :sparkles: versão inicial

--- src/backend/src/database/entities/User.ts
+++ src/backend/src/database/entities/User.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryColumn, CreateDateColumn, OneToOne, UpdateDateCo
 import { randomUUID } from "crypto";
 
 import { AppDataSource } from "../data-source";
-import { IUser } from "../../api/UseCases/User/IUser";
+
 @Entity("user")
 export class User {
 
@@ -33,90 +33,11 @@ export class User {
 	@UpdateDateColumn({type: "timestamp", nullable: false})
 	updated_at?: Date;
 
-	@ManyToOne(() => Role, role => role.user, {nullable: true})
-	@JoinColumn({name: "role_id"})
-	role : Role;
-
-	@OneToOne(() => Account_confirmation_status, account_confirmation_status => account_confirmation_status.user)
-	account_confirmation_status ?: Account_confirmation_status;
-
-	@OneToMany(() => Account_confirmation_pin_code, account_confirmation_pin_code => account_confirmation_pin_code.user)
-	account_confirmation_pin_code ?: Account_confirmation_pin_code;
-
 	@BeforeInsert()
 	setId() {
 
 		this.id = randomUUID();
 	
-	}
-
-	async isUserAlreadyRegistered(data: IUser) {
-
-		// Instacing a new user object.
-		const user = new User;
-
-		// Setting up the instancied user email address as the received email address.
-		user.email_address = data.email_address;
-
-		// Checking if user is already registered.
-		const isUserAlreadyRegistered = await AppDataSource.getRepository(User).findOneBy({email_address: user.email_address});
-
-		// Returning if user is already registered or not.
-		return Boolean(isUserAlreadyRegistered);
-
-	}
-
-	async store(data : IUser) {
-
-		// Instacing a new user object.
-		let user = new User;
-
-		// Setting up the instancied user email address as the received email address.
-		user.email_address = data.email_address;
-
-		// Instacing a new account confirmation status object.
-		const account_confirmation_status = new Account_confirmation_status;
-
-		// Setting up the main properties of account confirmation status object.
-		account_confirmation_status.is_email_address_confirmed = false;
-		account_confirmation_status.is_phone_number_confirmed = false;
-		account_confirmation_status.user = user;
-
-		// Assigning the user account confirmation status as the created account confirmation status.
-		user.account_confirmation_status = account_confirmation_status;
-
-		// Instacing a new role object.
-		const role = new Role;
-
-		// Setting up the main properties of role object.
-		role.id = (await AppDataSource.getRepository(Role).findOne({where: { name: "customer"}})).id;
-
-		// Setting up the user role as the instancied role object.
-		user.role = role;
-
-		// Saving the created user in the database.
-		user = await AppDataSource.getRepository(User).save(user);
-
-		// Saving the created account confirmation status in the database.
-		await AppDataSource.getRepository(Account_confirmation_status).save(account_confirmation_status);
-
-		// Returning the created user
-		return user;
-
-	}
-
-	async findById(id : string) {
-
-		// Getting the user with the entered id.
-		const data = await AppDataSource.getRepository(User).findOne({
-			where: {
-				id
-			},
-		});
-
-		// Returning the found data.
-		return data;
-
 	}
 
 }
