44548cd61a2235114e0d3b7eb0f6e87bd8e48dc1
Elias Biondo <contato@eliasbiondo.com>
2022-11-23 13:29:13 -0300

    :sparkles: adição de método para criar nova ordem de serviço

---
+++ src/backend/src/api/UseCases/Order_of_service/Interfaces/IOrder_of_service.ts
@@ -0,0 +1,14 @@
+import { IUser } from "../../User/Interfaces/IUser";
+
+export interface IOrder_of_service {
+	id: string,
+	vehicle_plate: string,
+	vehicle_color: string,
+	type: "entry" | "exit",
+	status: "pending" | "accepted" | "finished",
+	created_at?: Date,
+	accepted_at?: Date,
+	finished_at?: Date,
+	updated_at?: Date,
+	user: IUser
+}
--- src/backend/src/database/entities/Order_of_service.ts
+++ src/backend/src/database/entities/Order_of_service.ts
@@ -1,5 +1,7 @@
 import { randomUUID } from "crypto";
 import { BeforeInsert, Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { IOrder_of_service } from "../../api/UseCases/Order_of_service/Interfaces/IOrder_of_service";
+import { AppDataSource } from "../data-source";
 import { User } from "./User";
 
 @Entity("order_of_service")
@@ -39,6 +41,31 @@ export class Order_of_service {
 	setId() {
 
 		this.id = randomUUID();
+
+	}
+
+	async store(data : IOrder_of_service) {
+
+		// Instacing a new order of service object.
+		let order_of_service = new Order_of_service();
+
+		// Setting up the instancied order of service vehicle plate as the received vehicle plate.
+		order_of_service.vehicle_plate = data.vehicle_plate;
+
+		// Setting up the instancied order of service vehicle color as the received vehicle color.
+		order_of_service.vehicle_color = data.vehicle_color;
+
+		// Setting up the instancied order of service type as the received type.
+		order_of_service.type = data.type;
+
+		// Setting up the instancied order of service status as 'pending'.
+		order_of_service.status = "pending";
+		
+		// Saving the created order of service in the database.
+		order_of_service = await AppDataSource.getRepository(Order_of_service).save(order_of_service);
+
+		// Returning the created order of service.
+		return order_of_service;
 
 	}
 
