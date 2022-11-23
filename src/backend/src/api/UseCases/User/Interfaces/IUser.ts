d48a13e2b0ce9c140efd5bb08cb6f02232e4ae4e
Elias Biondo <contato@eliasbiondo.com>
2022-11-23 12:44:08 -0300

    :sparkles: adição de novos campos à entidade 'user'

--- src/backend/src/api/UseCases/User/Interfaces/IUser.ts
+++ src/backend/src/api/UseCases/User/Interfaces/IUser.ts
@@ -3,6 +3,13 @@ export interface IUser {
 	email: string,
 	password: string,
 	role: string,
-	created_at: Date,
-	updated_at: Date,
+	first_name: string,
+	last_name: string,
+	cpf: string,
+	identity_number: string,
+	date_of_birth: Date,
+	type_of_driver_license: string,
+	class: string,
+	created_at?: Date,
+	updated_at?: Date
 }
--- src/backend/src/database/entities/User.ts
+++ src/backend/src/database/entities/User.ts
@@ -19,6 +19,27 @@ export class User {
 	@Column({type: "varchar", nullable: false})
 	role: string;
 
+	@Column({type: "varchar", nullable: false})
+	first_name: string;
+
+	@Column({type: "varchar", nullable: false})
+	last_name: string;
+
+	@Column({type: "varchar", nullable: false})
+	cpf: string;
+
+	@Column({type: "varchar", nullable: false})
+	identity_number: string;
+
+	@Column({type: "date", nullable: false})
+	date_of_birth: Date;
+
+	@Column({type: "varchar", nullable: false})
+	type_of_driver_license: string;
+
+	@Column({type: "varchar", nullable: false})
+	class: string;
+
 	@CreateDateColumn({type: "timestamp", nullable: false})
 	created_at?: Date;
 
