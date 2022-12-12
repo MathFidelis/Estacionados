cddda2a6f9f2625d699ea84d65930162ac339d2a
Elias Biondo <contato@eliasbiondo.com>
2022-12-12 12:44:42 -0300

    :wrench: removendo permissões de acesso às ordens de serviço

--- src/backend/src/routes/orderOfServiceRoutes.ts
+++ src/backend/src/routes/orderOfServiceRoutes.ts
@@ -10,10 +10,10 @@ const router = Router();
 router.post("/", (req, res, next) => OrderOfServiceController.store(req, res, next));
 
 // Change to permit only valet.
-router.post("/accept/:id", (req, res, next) => auth(req, res, next), (req, res, next) => isUserAttendantOrManager(req, res, next), (req, res, next) => OrderOfServiceController.accept(req, res, next));
-router.post("/finish/:id", (req, res, next) => auth(req, res, next), (req, res, next) => isUserAttendantOrManager(req, res, next), (req, res, next) => OrderOfServiceController.finish(req, res, next));
-router.get("/", (req, res, next) => auth(req, res, next), (req, res, next) => isUserAttendantOrManager(req, res, next), (req, res, next) => OrderOfServiceController.list(req, res, next));
-router.get("/:id", (req, res, next) => auth(req, res, next), (req, res, next) => isUserAttendantOrManager(req, res, next), (req, res, next) => OrderOfServiceController.listOneById(req, res, next));
+router.post("/accept/:id", (req, res, next) => auth(req, res, next), (req, res, next) => OrderOfServiceController.accept(req, res, next));
+router.post("/finish/:id", (req, res, next) => auth(req, res, next), (req, res, next) => OrderOfServiceController.finish(req, res, next));
+router.get("/", (req, res, next) => auth(req, res, next), (req, res, next) => OrderOfServiceController.list(req, res, next));
+router.get("/:id", (req, res, next) => auth(req, res, next), (req, res, next) => OrderOfServiceController.listOneById(req, res, next));
 
 
 export default router;
