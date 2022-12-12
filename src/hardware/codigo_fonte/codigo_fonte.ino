1162af5d09709c6db393f89268cfa52e98411137
Elias Biondo <contato@eliasbiondo.com>
2022-12-12 14:41:10 -0300

    :wrench: ajustes no bloco central

--- src/hardware/codigo_fonte/codigo_fonte.ino
+++ src/hardware/codigo_fonte/codigo_fonte.ino
@@ -1,190 +1,293 @@
-// Incluindo bibliotecas necessárias à construção do sistema.
-#include <SPI.h>
-#include "WiFi.h"
-#include <Wire.h>
-#include <MFRC522.h>
+// Importing all required libraries to WiFi network and MQTT.
+#include <WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
 
-// Definindo as portas de saída de dispositivos comuns.
-#define OUTPUT_LED_R 36
-#define OUTPUT_LED_G 0
-#define OUTPUT_LED_B 45
-#define OUTPUT_BUZZER 20
+// String
+#include <String.h>
 
-// Definindo as portas do RFID.
+// Importing all required libraries to RFID reader.
+#include <SPI.h>
+#include <MFRC522.h>
+
+// Defining the RFID ports...
 #define RFID_SS_SDA 21
 #define RFID_RST 14
 
-// Definindo o rfid base.
+// Defining the RFID base...
 MFRC522 rfid = MFRC522(RFID_SS_SDA, RFID_RST);
 
-// Definindo o contador de etapas do dispositivo para controle interno.
-// Essa variável deve ser global porque pode ser utilizada em mais de um escopo de função.
-int etapa_atual = 0;
+// Defining other devices pin ports...
+#define OUTPUT_BUZZER 20
+#define OUTPUT_LED_1_R 36
+#define OUTPUT_LED_1_G 0
+#define OUTPUT_LED_1_B 45
+#define OUTPUT_LED_2_R 42
+#define OUTPUT_LED_2_G 40
+#define OUTPUT_LED_2_B 39
 
-// Instanciando as credenciais do ponto de acesso.
-const char *AP_FTM_SSID = "HelloWorld";
-const char *AP_FTM_PASS = "HelloWorld";
+// Defining the hardware stages...
+#define WAITING_VALET_LINK_RFID_TO_SET_ORDER_OF_SERVICE 0
+#define WAITING_ENTRANCE_SERVER_RESPONSE_WITH_THE_ORDER_OF_SERVICE_ID 1
+#define WAITING_CAR_PARKING 2
+#define CAR_PARKED 3
 
-// Instanciando as credenciais do broker MQTT.
+
+// Instancing the primary WiFi credentials.
+const char* WIFI_SSID = "iPhone";
+const char* WIFI_PASSWORD = "12345678";
+
+// Instancing the primary MQTT credentials.
 const char *BROKER_MQTT_HOST = "broker.hivemq.com";
 const char *BROKER_MQTT_CLIENT_ID = "7b6e3af9-fee4-4e9e-94ac-662e9020a17d";
 const int   BROKER_MQTT_PORT = 1883;
 
-// Instanciando variáveis para a transmissão MQTT.
+// Instancing the WiFi client.
 WiFiClient espClient;
+
+// Instancing the MQTT client.
 PubSubClient client(espClient);
 
-/*
- * Define as ações que devem ser realizadas ao receber uma mensagem MQTT.
- */
-void on_message(char* topic, byte* message, unsigned int length){
+// Instancing the current step variable to store at which stage of the flow we are in.
+int hardware_stage = WAITING_VALET_LINK_RFID_TO_SET_ORDER_OF_SERVICE;
 
-  // Imprimindo o tópico qual ela pertence...
-  Serial.println("Uma nova mensagem foi recebida no tópico ");
+// Intancing buffers to store the ids of orders of service to be linked to this device. 
+String entry_order_of_service_id = "";
+String exit_order_of_service_id = "";
+
+void setup() {
+
+  // Setting up the frequency of the serial port.
+  Serial.begin(115200);
+
+  // Printing a blank line.
+  Serial.println("");
+
+  // Settuping leds...
+  setup_leds();
+
+  // Setupping the WiFi network...
+  setup_wifi();
+
+  // Settuping the MQTT client...
+  setup_mqtt();
+
+  // Settuping the RFID...
+  setup_rfid();
+
+  // Setting the pin mode of buzzer as output.
+  pinMode(OUTPUT_BUZZER, OUTPUT);
+
+}
+
+void setup_leds() {
+
+  pinMode(OUTPUT_LED_1_R, OUTPUT);
+  pinMode(OUTPUT_LED_1_G, OUTPUT);
+  pinMode(OUTPUT_LED_1_B, OUTPUT);
+  pinMode(OUTPUT_LED_2_R, OUTPUT);
+  pinMode(OUTPUT_LED_2_G, OUTPUT);
+  pinMode(OUTPUT_LED_2_B, OUTPUT);
+
+  digitalWrite(OUTPUT_LED_1_R, LOW);
+  digitalWrite(OUTPUT_LED_1_G, HIGH);
+  digitalWrite(OUTPUT_LED_1_B, LOW);
+  digitalWrite(OUTPUT_LED_2_R, LOW);
+  digitalWrite(OUTPUT_LED_2_G, LOW);
+  digitalWrite(OUTPUT_LED_2_B, LOW);
+
+}
+
+void setup_wifi() {
+
+  // Waiting 10 miliseconds to avoid bugs...
+  delay(10);
+
+  // Printing the current system status...
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+
+  // Connecting to the primary WiFi network...
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // Printing the current system status...
+  while (WiFi.status() != WL_CONNECTED) {
+
+    delay(500);
+    Serial.print(".");
+
+  }
+
+  // Printing the current system status...
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.println("");
+
+  WiFi.SSID();
+
+  // Printing a blank line.
+  Serial.println("");
+
+}
+
+void setup_mqtt() 
+{
+
+  // Setting up the MQTT server.
+  client.setServer(BROKER_MQTT_HOST, BROKER_MQTT_PORT);
+
+  // Setting up the MQTT client callback.
+  client.setCallback(on_message);
+
+}
+
+void setup_rfid() 
+{
+
+  // Initializing the SPI barrament.
+	SPI.begin();
+
+	// Initializing the MFRC522.
+	rfid.PCD_Init();
+
+}
+
+
+void on_message(char* topic, byte* message, unsigned int length) 
+{
+
+  // Printing on receive a message...
+  Serial.print("\nMessage arrived on topic: ");
   Serial.print(topic);
   Serial.print(".");
-  Serial.println();
+
+
+  Serial.print("\nMessage: ");
+
+  String messageTemp;
+  
+  // Mouting the message...
+  for (int i = 0; i < length; i++) {
+    messageTemp += (char)message[i];
+  }
+
+  Serial.print(messageTemp);
+  Serial.println("");
+
+  if(hardware_stage == WAITING_ENTRANCE_SERVER_RESPONSE_WITH_THE_ORDER_OF_SERVICE_ID &&  strcmp(topic, "Estapar/OrdemDeServicoDeEntradaEncontrada") == 0) {
+
+    entry_order_of_service_id = messageTemp;
+
+    // Turning on LED 1 as blue.
+    digitalWrite(OUTPUT_LED_1_R, LOW);
+    digitalWrite(OUTPUT_LED_1_G, LOW);
+    digitalWrite(OUTPUT_LED_1_B, HIGH);
+
+    // Turning on LED 2 as yellow.
+    digitalWrite(OUTPUT_LED_2_R, HIGH);
+    digitalWrite(OUTPUT_LED_2_G, HIGH);
+    digitalWrite(OUTPUT_LED_2_B, LOW);
+
+    hardware_stage = WAITING_CAR_PARKING;
+
+  }
+
+  if(hardware_stage == WAITING_ENTRANCE_SERVER_RESPONSE_WITH_THE_ORDER_OF_SERVICE_ID && strcmp(topic, "Estapar/OrdemDeServicoDeEntradaNaoEncontrada") == 0) {
+
+    // Pulsing three times...
+    for(int i = 0; i < 3; i++) {
+
+      // Playing buzzer...
+      play_buzzer();
+
+      // Turning on LED 1 as red.
+      digitalWrite(OUTPUT_LED_1_R, HIGH);
+      digitalWrite(OUTPUT_LED_1_G, LOW);
+      digitalWrite(OUTPUT_LED_1_B, LOW);
+
+      // Waiting one second...
+      delay(1000);
+
+      // Turning off LED 1.
+      digitalWrite(OUTPUT_LED_1_R, LOW);
+      digitalWrite(OUTPUT_LED_1_G, LOW);
+      digitalWrite(OUTPUT_LED_1_B, LOW);
+
+      // Stopping buzzer...
+      stop_buzzer();
+
+      // Waiting one second...
+      delay(1000);
+
+    }
+
+    // Restarting device.
+    ESP.restart();
+
+  }
   
 }
 
-/*
- * Reconecta ao broker MQTT.
- */
-void reconnect() {
+void reconnect() 
+{
 
-  // Realizando um loop enquanto o cliente estiver desconectado.
+  // Loop until we're reconnected.
   while (!client.connected()) {
 
-    // Imprimindo o status atual do sistema.
-    Serial.print("Tentando conectar ao broker MQTT...");
+    // Printing the current system status.
+    Serial.println("Attempting MQTT connection...");
 
-    // Tentando conectar ao servidor.
+    // Attempting to connect.
     if (client.connect(BROKER_MQTT_CLIENT_ID)) {
 
-      Serial.println("Conectado!");
+      // Printing the current system status.
+      Serial.println("MQTT Connected!");
 
+      // Subscribing...
+      client.subscribe("Estapar/OrdemDeServicoDeEntradaEncontrada");
+      client.subscribe("Estapar/OrdemDeServicoDeEntradaNaoEncontrada");
+
+      // Printing a blank line.
+      Serial.println("");
+      
     } else {
 
-      // Imprimindo o status atual do sistema.
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" tentando novamente em 5 segundos...");
+      Serial.println("Trying again in 5 seconds...");
 
-      // Aguardando 5 segundos antes de tentar novamente.
+      // Wait 5 seconds before retrying
       delay(5000);
 
     }
   }
-}
-
-
-// -------------------------- Atenção --------------------------
-//
-// Início das configurações para medição de distância - <<<FTM>>>.
-//
-// -------------------------------------------------------------
-
-// Configurando os períodos.
-const uint8_t FTM_FRAME_COUNT = 16;
-const uint16_t FTM_BURST_PERIOD = 2;
-
-// Instanciando um semáforo para sinalizar quando um relatório FTM for recebido.
-xSemaphoreHandle sinalizadorFTM;
-
-// Instanciando variável para armazenar o status do relatório FTM a ser recebido.
-bool status_do_relatorio_FTM = true;
-
-// Instanciando variável para armazenar a distância calculada.
-float distancia_do_roteador_em_metros = 0.0;
-
-/*
- * Calcula a distância estimada quando um relatório ftm for recebido.
- */
-void ao_receber_um_relatorio_FTM(arduino_event_t *event) {
-
-  // Instanciando os status possíveis de resposta.
-  const char * status_str[5] = {"SUCCESS", "UNSUPPORTED", "CONF_REJECTED", "NO_RESPONSE", "FAIL"};
-
-  // Instanciando um ponteiro para o relatório.
-  wifi_event_ftm_report_t * report = &event -> event_info.wifi_ftm_report;
-
-  // Definindo o status global do relatório.
-  status_do_relatorio_FTM = report -> status == FTM_STATUS_SUCCESS;
-
-  // Controlando o fluxo através do status atual do relatório.
-  if (status_do_relatorio_FTM) {
-
-    // Armazenando a distância calculada.
-    distancia_do_roteador_em_metros = (report -> dist_est / 100.0) - 40.0;
-
-    // Limpando a memória após o uso das variáveis.
-    free(report->ftm_report_data);
-
-  } else {
-
-    // Imprimindo o erro.
-    Serial.println("Relatório FTM negado: ");
-    Serial.println(status_str[report->status]);
-
-  }
-
-  // Informando que um semáforo foi recebido.
-  xSemaphoreGive(sinalizadorFTM);
 
 }
 
-/*
- * Iniciando uma sessão FTM e solicitando um relatório FTM.
- */
-bool solicitar_um_relatorio_FTM(){
-
-  // Controlando possíveis erros.
-  if(!WiFi.initiateFTM(FTM_FRAME_COUNT, FTM_BURST_PERIOD)){
-
-    // Informando a falha ao iniciar sessão.
-    Serial.println("ERRO FTM: Falha ao iniciar sessão.");
-
-    // Retornando uma resposta negativa.
-    return false;
-
-  }
-
-  // Aguardando o sinal que o relatório foi recebido com sucesso e retornando verdadeiro.
-  return xSemaphoreTake(sinalizadorFTM, portMAX_DELAY) == pdPASS && status_do_relatorio_FTM;
-
-}
-
-// -------------------------- Atenção --------------------------
-//
-// Fim das configurações para medição de distância - <<<FTM>>>.
-//
-// -------------------------------------------------------------
-
-/*
- * Toca um som de operação realizada com sucesso no buzzer da aplicação,
- * como feedback para o usuário.
- */
-void tocar_som_de_operacao_realizada()
+void play_buzzer()
 {
 
-  // Tocando um som de 300Hz durante 1 segundo.
+  // Playing a song of 300Hz...
 	tone(OUTPUT_BUZZER, 3000);
-	delay(1000);
+
+}
+
+void stop_buzzer()
+{
+
+  // Interrupting the song...
 	noTone(OUTPUT_BUZZER);
 
 }
 
-/*
- * Captura o identificador de um cartão RFID.
- */
-void capturar_RFID()
+void rfid_captor()
 {
 
-	// Instanciando um espaço na memória para armazenar a tag RFID lida.
+  // Instancing a memory space to store the RFID read.
 	String strID = "";
 
-	// Gerando o número da tag RFID lida.
+	// Generating the number of RFID tag....
 	for (byte i = 0; i < 4; i++)
 	{
 		strID +=
@@ -193,207 +296,97 @@ void capturar_RFID()
 			(i != 3 ? ":" : "");
 	}
 
-	// Transformando todo o texto em maiúsculo.
+  // Transforming the text to uppercase...
 	strID.toUpperCase();
 
-	// Imprimindo o identificado lido.
-	Serial.print("Identificador da tag: ");
+  // Printing the RFID read.
+	Serial.print("RFID Tag: ");
 	Serial.println(strID);
 
-	// Parando a leitura do cartão.
+  char *nStr = const_cast<char*>(strID.c_str());
+
+  if(hardware_stage == WAITING_VALET_LINK_RFID_TO_SET_ORDER_OF_SERVICE) {
+
+    client.publish("Estapar/VincularOrdemDeServicoDeEntradaComBlocoCentral", nStr);
+
+  }
+  
+	// Stopping the reading...
 	rfid.PICC_HaltA();
 
-	// Parando a criptografia no PCD.
+  // Stopping PCD cryptografy...
 	rfid.PCD_StopCrypto1();
 
-  tocar_som_de_operacao_realizada();
+  // Playing buzzer...
+  play_buzzer();
+
+  // Waiting 1 second...
+  delay(1000);
+
+  // Stopping buzzer.
+  stop_buzzer();
 
 }
 
-/*
- * Inicia a operação do veículo.
- */
-void operacao_iniciada()
-{
 
-	// Acendendo o LED na cor vermelha.
-	digitalWrite(OUTPUT_LED_G, LOW);
-	digitalWrite(OUTPUT_LED_B, LOW);
-	digitalWrite(OUTPUT_LED_R, HIGH);
+void loop() {
 
-	// Tocando som de operação realizada com sucesso.
-	tocar_som_de_operacao_realizada();
+  // If client is not connected.
+  if (!client.connected()) {
 
-	// Delay...
-	delay(3000);
+    // Reconnecting...
+    reconnect();
 
-	etapa_atual++;
+  }
 
-}
+  // Listen MQTT broker.
+  client.loop();
 
-/*
- * Informa que o carro foi estacionado.
- */
-void carro_estacionado()
-{
+  // Caso não haja um cartão RFID próximo, ou se um cartão RFID permanecer próximo ao sensor, ignorando uma nova leitura...
 
-	// Acendendo o LED na cor amarela.
-	digitalWrite(OUTPUT_LED_G, HIGH);
-	digitalWrite(OUTPUT_LED_B, HIGH);
-	digitalWrite(OUTPUT_LED_R, HIGH);
-
-	// Tocando som de operação realizada com sucesso.
-	tocar_som_de_operacao_realizada();
-
-	etapa_atual++;
-
-}
-
-/*
- * Informa que o carro foi entregue e finaliza a operação.
- */
-void carro_entregue()
-{
-
-	// Tocando som de operação realizada com sucesso.
-	tocar_som_de_operacao_realizada();
-
-	// Pulsando LEDs...
-	digitalWrite(OUTPUT_LED_R, HIGH);
-	digitalWrite(OUTPUT_LED_G, LOW);
-	digitalWrite(OUTPUT_LED_B, HIGH);
-	delay(1000);
-	digitalWrite(OUTPUT_LED_R, LOW);
-	digitalWrite(OUTPUT_LED_G, HIGH);
-	digitalWrite(OUTPUT_LED_B, HIGH);
-	delay(1000);
-	digitalWrite(OUTPUT_LED_R, HIGH);
-	digitalWrite(OUTPUT_LED_G, HIGH);
-	digitalWrite(OUTPUT_LED_B, LOW);
-
-	// Delay...
-	delay(2500);
-
-	// Reiniciando o dispositivo.
-	ESP.restart();
-
-}
-
-/*
- * Instancia as principais configurações da aplicação, além dos dispositivos de entrada e saída.
- */
-void setup()
-{
-
-	// Definindo a frequência do serial.
-	Serial.begin(115200);
-
-	// Definindo os LEDs.
-	pinMode(OUTPUT_LED_R, OUTPUT);
-	pinMode(OUTPUT_LED_G, OUTPUT);
-	pinMode(OUTPUT_LED_B, OUTPUT);
-	pinMode(OUTPUT_BUZZER, OUTPUT);
-
-	// Inicializando o barramento SPI.
-	SPI.begin();
-
-	// Inicializando o MFRC522.
-	rfid.PCD_Init();
-
-	// Acendendo o LED verde no momento de inicialização do dispositivo.
-	digitalWrite(OUTPUT_LED_R, LOW);
-	digitalWrite(OUTPUT_LED_G, HIGH);
-	digitalWrite(OUTPUT_LED_B, LOW);
-
-	// Imprimindo o status atual do sistema.
-	Serial.print("\nConectando ao roteador wi-fi");
-
-	// Conectando ao roteador wi-fi...
-	WiFi.begin(AP_FTM_SSID, AP_FTM_PASS);
-
-	// Imprimindo um ponto no serial enquanto o wi-fi não conecta.
-	while (WiFi.status() != WL_CONNECTED)
-	{
-
-    // Conectando ao roteador wi-fi...
-		Serial.print("...");
-		delay(500);
-    
-	}
-
-	// Imprimindo uma linha no serial.
-	Serial.println("");
-
-	// Imprimindo o status atual do sistema.
-	Serial.println("Wi-fi conectado!");
-
-  // Definindo o servidor MQTT.
-  client.setServer(BROKER_MQTT_HOST, BROKER_MQTT_PORT);
-  client.setCallback(on_message);
-
-  // -------------------------- Atenção --------------------------
-  //
-  // Início das configurações para medição de distância - <<<FTM>>>.
-  //
-  // -------------------------------------------------------------
-
-  // Criando um semáforo binário.
-  sinalizadorFTM = xSemaphoreCreateBinary();
-
-  // Escutando eventos de relatórios FTM.
-  WiFi.onEvent(ao_receber_um_relatorio_FTM, ARDUINO_EVENT_WIFI_FTM_REPORT);
-
-  // Informando o status atual do sistema.
-  Serial.println("Inicializando sessão FTM...");
-
-
-  // -------------------------- Atenção --------------------------
-  //
-  // Fim das configurações para medição de distância - <<<FTM>>>.
-  //
-  // -------------------------------------------------------------
-
-}
-
-/*
- * Controla a aplicação a cada instante de tempo.
- */
-void loop()
-{
-
-  solicitar_um_relatorio_FTM();
-  Serial.printf("Distância atual: %.2f m\n", (float) distancia_do_roteador_em_metros);
-
-	// Caso não haja um cartão RFID próximo, ou se um cartão RFID permanecer próximo ao sensor, ignorando uma nova leitura...
+  // If has not RFID card nearby, or the RFID stay nearby the sensor, ignoring a new read...
 	if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
 	{
 
 		return;
+    
 	}
 	else
 	{
 
-		// Capturando um novo RFID.
-		capturar_RFID();
+		// Capturing a new RFID...
+		rfid_captor();
 
-		// Se a etapa atual for 0...
-		if (etapa_atual == 0)
-		{
+    if(hardware_stage == WAITING_VALET_LINK_RFID_TO_SET_ORDER_OF_SERVICE) {
 
-			// Iniciando a operação do veículo...
-			operacao_iniciada();
-		}
-		else if (etapa_atual == 1)
-		{
+      // Setting the current step as 1.
+      hardware_stage = WAITING_ENTRANCE_SERVER_RESPONSE_WITH_THE_ORDER_OF_SERVICE_ID;
 
-			// Informando que o carro foi estacionado...
-			carro_estacionado();
-		}
-		else if (etapa_atual == 2)
-		{
+    } else if (hardware_stage == WAITING_CAR_PARKING) {
 
-			// Informando que o carro foi entregue e finalizando a operação...
-			carro_entregue();
-		}
-	}
+      // Keeping LED 1 as blue.
+      digitalWrite(OUTPUT_LED_1_R, LOW);
+      digitalWrite(OUTPUT_LED_1_G, LOW);
+      digitalWrite(OUTPUT_LED_1_B, HIGH);
+
+      // Turning on LED 2 as white.
+      digitalWrite(OUTPUT_LED_2_R, HIGH);
+      digitalWrite(OUTPUT_LED_2_G, HIGH);
+      digitalWrite(OUTPUT_LED_2_B, HIGH);
+
+      // Converting the string to a array of chars...
+      char *orderOfServiceIdStr = const_cast<char*>(entry_order_of_service_id.c_str());
+
+      // Publishing that car is parked...
+      client.publish("Estapar/CarroEstacionado", orderOfServiceIdStr);
+
+      hardware_stage = CAR_PARKED;
+
+
+    }
+    
+
+  }
+    
+
 }
